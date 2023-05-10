5e11f3349075f8d84fd8f8d83f3829d8968d494f
enzonjagi <amosndungo@gmail.com>
2023-05-10 16:04:21 +0300

    A feature to allow the admin to create shops on the platform - Only availabe on the admin side of things

--- shops/admin.py
+++ shops/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from django.contrib.gis.admin import OSMGeoAdmin
+from .models import Shop
 
-# Register your models here.
+@admin.register(Shop)
+class ShopAdmin(OSMGeoAdmin):
+    """Admin entry for the Shop"""
+
+    list_display = ('name', 'location')
