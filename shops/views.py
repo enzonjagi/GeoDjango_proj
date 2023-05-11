86e58e18ed3fb508f401869ef0adefe809fbe943
enzonjagi <amosndungo@gmail.com>
2023-05-11 10:28:42 +0300

    Created a view that is expected to sho a list of Nearby shops, however, it is not showing at this point in time.

--- shops/views.py
+++ shops/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from django.views import generic
+from django.contrib.gis.geos import fromstr, Point
+from django.contrib.gis.db.models.functions import Distance
+from .models import Shop
 
-# Create your views here.
+# we're hard coding the coordinates of Nairobi 
+# This is to simulate a user's location. 
+# Ideally the user should supply this info, 
+# but we're doign this as a demo.
+
+# for miami data
+longitude = -80.191_788
+latitude = 25.761_681
+
+user_location = Point(longitude, latitude, srid=4326)
+
+# for Nairobi data
+longitude2 = -36.8219
+latitude2 = 1.2921
+
+user_location2 = Point(longitude2, latitude2, srid=4326)
+# user_location = fromstr(f'POINT({longitude} {latitude})', srid=4326)
+
+class Home(generic.ListView):
+    """A view of shops around the user's location"""
+
+    model = Shop
+    content_object_name = 'shops'
+    queryset = Shop.objects.annotate(
+        distance=Distance('location', user_location2)
+    ).order_by("distance")[0:6]
+    template_name = 'shops/index.html'
+
+home = Home.as_view()
